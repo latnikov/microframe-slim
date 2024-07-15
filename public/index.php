04287e838398b395e9203cdd3efdd2dcd71d7ae0
Maxim Latnikov <latnikou@gmail.com>
2024-07-15 14:23:50 +0300

    added new package

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
+use Slim\Factory\AppFactory;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
